dc1ef0c672aae0acb95aba61391ea0ccdeae5c77
Bartosz Klimowicz <klimowicz.bartek@gmail.com>
2025-02-06 22:04:17 +0100

    Some updates

--- HomeStorage.Domain/Model/Location.cs
+++ HomeStorage.Domain/Model/Location.cs
@@ -4,7 +4,7 @@ public class Location
 {
     public string Name { get; }
     public IEnumerable<Product> Products => _products;
-    private IList<Product> _products;
+    private readonly IList<Product> _products;
     
     public Location(string name, IEnumerable<Product>? products)
     {
@@ -13,5 +13,21 @@ public class Location
         {
             _products = new List<Product>();
         }
+    }
+    
+    public void AddProduct(Product product)
+    {
+        _products.Add(product);
+    }
+    
+    public void RemoveProduct(Product product)
+    {
+        _products.Remove(product);
+    }
+    
+    public void MoveProductToNewLocation(Product product, Location newLocation)
+    {
+        _products.Remove(product);
+        newLocation.AddProduct(product);
     }
 }
--- HomeStorage.Domain/Model/Product.cs
+++ HomeStorage.Domain/Model/Product.cs
@@ -2,22 +2,17 @@ using HomeStorage.Domain.Exceptions;
 
 namespace HomeStorage.Domain.Model;
 
-public class Product
+public class Product(string name, Location location, decimal quantity, string? producer, string? comments = null)
 {
-    public string Name { get; private set; }
-    public Location Location { get; private set; }
-    public decimal Quantity { get; private set; }
-    public string? Comments { get; set; }
-    public string Producer { get; set; }
-    
-    public Product(string name, Location location, decimal quantity, string? comments = null)
-    {
-        Name = string.IsNullOrWhiteSpace(name) ? throw new InvalidNameException(name) : name;
-        Location = location ?? throw new InvalidLocationException();
-        Quantity = quantity < 0 ? throw new InvalidQuantityException(quantity) : quantity;
-        Comments = comments;
-    }
+    public string Name { get; private set; } = string.IsNullOrWhiteSpace(name) ? throw new InvalidNameException(name) : name;
+    public Location Location { get; private set; } = location ?? throw new InvalidLocationException();
+    public decimal Quantity { get; private set; } = quantity < 0 ? throw new InvalidQuantityException(quantity) : quantity;
+    public string? Comments { get; set; } = comments;
+    public string? Producer { get; set; } = producer;
 
+    public static Product Create(string name, Location location, decimal quantity, string? producer, string? comments = null) => 
+        new(name, location, quantity, producer, comments);
+    
     public void UpdateQuantity(decimal quantity)
     {
         if (quantity < 0)
